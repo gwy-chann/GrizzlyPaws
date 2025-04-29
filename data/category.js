f6ee62b4840a23613a6e66453aaefda3e500b989
gwy-chann <izzah.tabuena@gmail.com>
2025-04-29 20:48:30 +0800

    IP: lizbeth products

---
+++ data/category.js
@@ -0,0 +1,59 @@
+const category = [
+  {
+    id: 1, name: "Dog", link: "/category/dog.html",
+  },
+  {
+    id: 2, name: "Cat", link: "/category/cat.html",
+  },
+  {
+    id: 3, name: "Small Pet", link: "/category/small_pet.html",
+  },
+  {
+    id: 4, name: "Feathered", link: "/category/feathered.html",
+  },
+  {
+    id: 5, name: "Aquatic", link: "/category/aquatic.html",
+  },
+];
+
+
+const pageDetails = {
+    dog: {
+        title: "Dog Shop: Dog Supplies & Dog Accessories",
+        description: `We offer a wide range of dog food and treats and dog accessories. Here you'll find all brands of <span class="highlight">dog food</span>, <span class="highlight">dog kennels</span>, <span class="highlight">dog beds</span>, <span class="highlight">dog leads</span>, <span class="highlight">dog training equipment</span>, and much more.`,
+    },
+    cat: {
+        title: "Cat Shop: Dog Supplies & Dog Accessories",
+        description: `We offer a wide range of dog food and treats and dog accessories. Here you'll find all brands of <span class="highlight">dog food</span>, <span class="highlight">dog kennels</span>, <span class="highlight">dog beds</span>, <span class="highlight">dog leads</span>, <span class="highlight">dog training equipment</span>, and much more.`,
+    }
+};
+
+const subCategory = {
+  dog: [
+    { link: '../category products/dog-dry-food-products.html', id: 1, img: "", name: "Dry Dog Food" },
+    { link: '#', id: 2, img: "", name: "Wet Dog Food" },
+    { link: '#', id: 3, img: "", name: "Dog Treats & Dog Bones" },
+    { link: '#', id: 4, img: "", name: "Dog Supplements & Special Food" },
+    { link: '#', id: 5, img: "", name: "Dog Kennels, Dog Flaps & Gates" },
+    { link: '#', id: 6, img: "", name: "Dog Crates & Dog Travel" },
+    { link: '#', id: 7, img: "", name: "Dog Beds & Baskets" },
+    { link: '#', id: 8, img: "", name: "Dog Toys, Sports & Training" },
+    { link: '#', id: 9, img: "", name: "Dog Grooming & Care" },
+    { link: '#', id: 10, img: "", name: "Dog Leads & Dog Collars" },
+  ],
+cat: [
+    { link: '#', id: 1, img: "", name: "Dry Cat Food" },
+    { link: '#', id: 2, img: "", name: "Wet Cat Food" },
+    { link: '#', id: 3, img: "", name: "Cat Treats & Cat Bones" },
+    { link: '#', id: 4, img: "", name: "Cat Supplements & Special Food" },
+    { link: '#', id: 5, img: "", name: "Cat Kennels, Cat Flaps & Gates" },
+    { link: '#', id: 6, img: "", name: "Cat Crates & Cat Travel" },
+    { link: '#', id: 7, img: "", name: "Cat Beds & Baskets" },
+    { link: '#', id: 8, img: "", name: "Cat Toys, Sports & Training" },
+    { link: '#', id: 9, img: "", name: "Cat Grooming & Care" },
+    { link: '#', id: 10, img: "", name: "Cat Leads & Cat Collars" },
+],
+};
+
+
+
